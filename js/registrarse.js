a37bf15e69d8102946b09bf09c3dd6368dd4eee4
Martín Calderón <elmcwl@gmail.com>
2020-11-17 04:54:30 -0700

    1
    1

---
+++ js/registrarse.js
@@ -0,0 +1,12 @@
+function registrarse() {   
+    var email = document.getElementById('email').value;    
+    var password = document.getElementById('password').value;    
+    var nombre = document.getElementById('nombre').value;    
+    var apellido = document.getElementById('apellido').value;     
+
+    localStorage.setItem(email, password, nombre, apellido);
+    alert("Su cuenta ha sido registrada");   
+    document.getElementById("registrarse").reset();
+    location.href = 'index.html';
+     
+}
